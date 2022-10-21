47eb0ab23c9226b1764b1f99e56d71e352be8c72
aeong98 <sy9815@gmail.com>
2022-10-21 23:10:28 +0900

    [ch1] 문제 해결

--- 김서연/CH1. /src/index.ts
+++ 김서연/CH1. /src/index.ts
@@ -1,19 +1,17 @@
-
 interface Invitation{
     when: Date;
 }
 
-function Invitation(this:any, when: Date){
+function Invitation(this:Invitation, when: Date){
     this.when =when;
 }
-
 
 interface Ticket{
     fee: number;
     getFee : ()=>number;
 }
 
-function Ticket(this:any, fee:number){
+function Ticket(this:Ticket, fee:number){
     this.fee = fee;
 
     this.getFee = ()=>{ 
@@ -26,6 +24,7 @@ interface Bag {
     amount: number;
     invitation : Invitation;
     ticket: Ticket;
+    hold: (ticket:Ticket)=>number;
     hasInvitation : ()=> boolean;
     hasTicket : ()=>boolean;
     setTicket: (ticket:Ticket)=>void;
@@ -33,10 +32,21 @@ interface Bag {
     plusAmount: (amount:number)=>void;
 }
 
-function Bag(this:any, amount:number, invitation:Invitation,  ticket?:Ticket){
+function Bag(this:Bag, amount:number, invitation:Invitation,  ticket:Ticket){
     this.amount=amount;
     this.invitation=invitation;
     this.ticket=ticket;
+
+    this.hold=(ticket:Ticket)=>{
+        if(this.hasInvitation()){
+            this.setTicket(ticket);
+            return 0;
+        }else{
+            this.setTicket(ticket);
+            this.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
 
     this.hasInvitation = () => invitation!==null;
     this.hasTicket = ()=> ticket!==null;
@@ -54,12 +64,14 @@ function Bag(this:any, amount:number, invitation:Invitation,  ticket?:Ticket){
 interface Audience{
     bag:Bag;
     getBag: ()=>Bag;
+    buy: (ticket:Ticket)=>number;
 }
 
-function Audience(this:any, bag:Bag){
+function Audience(this:Audience, bag:Bag){
     this.bag= bag;
 
     this.getBag = ()=> this.bag;
+    this.buy =(ticket: Ticket)=> this.bag.hold(ticket);
 }
 
 interface TicketOffice{
@@ -68,48 +80,51 @@ interface TicketOffice{
     getTicket: ()=>Ticket;
     minusAmount: (amount:number)=>void;
     plusAmount: (amount:number)=>void;
+    sellTicketTo: (audience: Audience)=> void;
 }
 
 
-function TicketOffice(this:any, amount:number, ...tickets:Ticket[]){
+function TicketOffice(this:TicketOffice, amount:number, ...tickets:Ticket[]){
     this.amount = amount;
     this.tickets = [...tickets];
 
-    this.getTicket=()=> tickets.shift();
+    this.getTicket=()=> tickets.shift()!;
     this.minusAmount=(amount:number)=>{
         this.amount -= amount;
     }
     this.plusAmount=(amount:number)=>{
         this.amount+=amount;
     }
+    this.sellTicketTo=(audience: Audience)=>{
+        this.plusAmount(audience.buy(this.getTicket()))
+    }
 }
 
 
 interface TicketSeller {
     ticketOffice: TicketOffice;
-    getTicketOffice: ()=>TicketOffice;
+    sellTo: (audience:Audience)=>void;
 }
 
-function TicketSeller(this:any, ticketOffice : TicketOffice){
+function TicketSeller(this:TicketSeller, ticketOffice : TicketOffice){
     this.ticketOffice= ticketOffice;
 
-    this.getTicketOffice =() => this.ticketOffice;
+    this.sellTo=(audience:Audience)=>{
+        ticketOffice.sellTicketTo(audience);
+    }
 }
 
+
+interface Theater{
+    ticketSeller: TicketSeller;
+    enter: (audience: Audience)=>void;
+}
 
 
 function Theater(this:any, ticketSeller: TicketSeller){
     this.ticketSeller=ticketSeller;
     
     this.enter=(audience:Audience)=>{
-        if(audience.getBag().hasInvitation()){
-            let ticket = this.ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        }else{
-            let ticket= this.ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
     }
 }
